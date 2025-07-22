5ab58d82b59430d1aed00053528a469433e6a0ff
Muhammad Dzulfiqar <muhammaddzulfiqar03@gmail.com>
2025-07-22 10:10:16 +0700

    D-Scrapper - Developing

---
+++ databases/mysql.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+def simpan_ke_mysql(nama_lengkap, company):
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",        # Ganti sesuai server MySQL kamu
+            user="root",             # Ganti sesuai user MySQL kamu
+            password="",   # Ganti dengan password usermu
+            database="db_dscrapper"  # Nama database
+        )
+        cursor = conn.cursor()
+        sql = "INSERT INTO db_dscrapper (nama_lengkap, company) VALUES (%s, %s)"
+        val = (nama_lengkap, company)
+        cursor.execute(sql, val)
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print("Gagal simpan ke MySQL:", e)
