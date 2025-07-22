5a0f59f29200fa144f6d902b7ea31273f8a5a9d0
Muhammad Dzulfiqar <muhammaddzulfiqar03@gmail.com>
2025-07-22 09:29:18 +0700

    D-Scrapper - Developing

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("app.html")
+    return render_template("index.html")
 
 @app.route("/run-script", methods=["POST"])
 def run_script():
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  root: 'templates', // Direktori tempat file index.html berada
+  build: {
+    outDir: 'dist', // Folder hasil build
+    emptyOutDir: true,
+  },
 })
